3f86c22bc0771dbd62d96499285e73f7ed80553c
Chris Grekas <134630911+chrisgrekas@users.noreply.github.com>
2026-05-08 23:11:29 +0300

    Added more cases for testing
    Added more testing cases in main

--- main.py
+++ main.py
@@ -23,3 +23,22 @@ from services.services import get_all_calls,get_call_by_id,archive_call,unarchiv
 #     print(filter_calls(is_archived=True))
 # except TypeError as e:
 #     print(f"Error: {e}")
+
+# print(unarchive_call("3"))
+
+# try:
+#     print(unarchive_call("999"))
+# except ValueError as e:
+#     print(f"Error: {e}")
+
+# print(get_call_by_id("4"))
+
+# try:
+#     print(add_note_to_call("999", "Test note"))
+# except ValueError as e:
+#     print(f"Error: {e}")
+
+# try:
+#     print(filter_calls(is_archived=1))
+# except TypeError as e:
+#     print(f"Error: {e}")
