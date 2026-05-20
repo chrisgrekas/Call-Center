57ab30b22388a60a015d18cd49b1108c82074809
Chris Grekas <134630911+chrisgrekas@users.noreply.github.com>
2026-05-20 23:36:59 +0300

    Fix error responses and status codes based on code review
    Fix error responses and status codes based on code review

--- main.py
+++ main.py
@@ -55,23 +55,23 @@ def get_calls(call_type: str = None, direction: str = None, is_archived: bool = 
         try:
             return {"calls": [call_to_dict(call) for call in filter_calls(call_type, direction, is_archived)]}
         except ValueError as e:
-            raise HTTPException(status_code=404 , detail= "Something went wrong")
+            raise HTTPException(status_code=400 , detail= str(e))
         except TypeError as e:
-            raise HTTPException(status_code=404 , detail= "Something went wrong")
+            raise HTTPException(status_code=400 , detail= str(e))
 @app.get("/calls/{call_id}")
 def get_call_by_the_id(call_id:str):
     try:
         return call_to_dict(get_call_by_id(call_id))
     except ValueError as e:
         logger.warning(f"Call {call_id} not found")
-        raise HTTPException(status_code= 404, detail="Call not found")
+        raise HTTPException(status_code= 404, detail=str(e))
 @app.patch("/calls/{call_id}/archive")
 def archive_call_by_id(call_id:str):
     try:
         return call_to_dict(archive_call(call_id))
     except ValueError as e:
         logger.warning(f"Call {call_id} not found")
-        raise HTTPException(status_code= 404 , detail="Something went wrong")
+        raise HTTPException(status_code= 404 , detail=str(e))
 
 @app.patch("/calls/{call_id}/unarchive")
 def unarchive_call_by_id(call_id:str):
@@ -79,7 +79,7 @@ def unarchive_call_by_id(call_id:str):
         return call_to_dict(unarchive_call(call_id))
     except ValueError as e:
         logger.warning(f"Call {call_id} not found")
-        raise HTTPException(status_code= 404 , detail="Something went wrong")
+        raise HTTPException(status_code= 404 , detail=str(e))
 class NoteRequest(BaseModel):
     content: str
 @app.post("/calls/{call_id}/notes")
@@ -88,7 +88,7 @@ def add_node(call_id: str , note : NoteRequest):
         return call_to_dict(add_note_to_call(call_id,note.content))
     except ValueError as e:
         logger.warning(f"Call {call_id} not found")
-        raise HTTPException(status_code= 404 , detail="Something went wrong")
+        raise HTTPException(status_code= 404 , detail=str(e))
 
 class CreateCallBody(BaseModel):
     direction: str
@@ -103,4 +103,4 @@ def createCall(body : CreateCallBody):
         return call_to_dict(create_call(body.direction , body.from_, body.to_ , body.call_type , body.duration , body.is_archived))
     except ValueError as e:
         logger.warning(f"Invalid Body")
-        raise HTTPException(status_code=400 , detail="Something went wrong")
+        raise HTTPException(status_code=400 , detail=str(e))
