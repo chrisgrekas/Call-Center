1d3794dcb493233020f39a29cc05ff184233fd0e
Chris Grekas <134630911+chrisgrekas@users.noreply.github.com>
2026-05-22 23:11:47 +0300

    Added :fixture in conftest.py that resets the calls.json
    Added a fixture in conftest.py that resets the calls.json

--- conftests.py
+++ conftests.py
@@ -1,5 +1,11 @@
 import sys
 import os
+import shutil
+import pytest
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from validators.validators import validate_call_types
+@pytest.fixture(autouse=True)
+def reset_data():
+    shutil.copy("tests/calls_backup.json", "data/calls.json")
+    yield
