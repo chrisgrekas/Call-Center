baa4ec1ba71223dec38a1026aab649e8e45ed612
Chris Grekas <134630911+chrisgrekas@users.noreply.github.com>
2026-05-23 10:19:44 +0300

    Update validators.py
    just for checking the github actions

--- validators/validators.py
+++ validators/validators.py
@@ -2,7 +2,7 @@ def validate_call_types(call_types):
     if (call_types == "answered") or (call_types == "missed") or (call_types == "voicemail"):
         return call_types
     else:
-        raise ValueError(f"Invalid: {call_types}. Please enter a valid type of call. (answered, missed, voicemail)")
+        raise ValueError(f"Invalid: {call_types}. Please enter a valid type of call. (answered, missed, voicemail)!")
 
 def validate_direction(direction):
     if (direction == "inbound") or (direction == "outbound"):
